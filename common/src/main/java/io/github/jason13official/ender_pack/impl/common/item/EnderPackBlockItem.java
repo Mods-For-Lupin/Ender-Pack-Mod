8f1610503fee9810c4d3f3b7b29d1282d9fef9da
jason13official <jason13official@gmail.com>
2026-04-26 21:37:59 -0400

    item properties

---
+++ common/src/main/java/io/github/jason13official/ender_pack/impl/common/item/EnderPackBlockItem.java
@@ -0,0 +1,11 @@
+package io.github.jason13official.ender_pack.impl.common.item;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class EnderPackBlockItem extends BlockItem {
+
+  public EnderPackBlockItem(Block block, Properties properties) {
+    super(block, properties);
+  }
+}
--- common/src/main/java/io/github/jason13official/ender_pack/impl/common/registry/ModItems.java
+++ common/src/main/java/io/github/jason13official/ender_pack/impl/common/registry/ModItems.java
@@ -2,13 +2,20 @@ package io.github.jason13official.ender_pack.impl.common.registry;
 
 import io.github.jason13official.ender_pack.Constants;
 import io.github.jason13official.ender_pack.EnderPack;
+import io.github.jason13official.ender_pack.impl.common.item.EnderPackBlockItem;
 import java.util.function.BiConsumer;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.BlockItem;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.equipment.ArmorMaterials;
+import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.equipment.Equippable;
 
 public class ModItems {
 
@@ -16,7 +23,12 @@ public class ModItems {
 
   public static void register(BiConsumer<Item, Identifier> consumer) {
 
-    ENDER_PACK = new BlockItem(ModBlocks.ENDER_PACK, new Properties().setId(ResourceKey.create(Registries.ITEM, EnderPack.identifier(Constants.MOD_ID))));
+    ENDER_PACK = new EnderPackBlockItem(ModBlocks.ENDER_PACK, new Properties()
+        .attributes(ArmorMaterials.LEATHER.createAttributes(ArmorType.CHESTPLATE)).enchantable(ArmorMaterials.LEATHER.enchantmentValue())
+        .component(DataComponents.EQUIPPABLE, Equippable.builder(EquipmentSlot.CHEST).setEquipSound(SoundEvents.ARMOR_EQUIP_LEATHER).build())
+        .repairable(Items.PHANTOM_MEMBRANE)
+        .setId(ResourceKey.create(Registries.ITEM, EnderPack.identifier(Constants.MOD_ID))));
+
     consumer.accept(ENDER_PACK, EnderPack.identifier(Constants.MOD_ID));
   }
 }
